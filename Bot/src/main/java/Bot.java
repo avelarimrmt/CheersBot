60c8dc25dc7fb1fca79c8522e9244944a263be05
Alessa666 <as2010alisa@gmail.com>
2020-04-11 14:04:34 +0500

    Кнопки на клавиатуре
    Перечисление, для использующихся в сообщениях и кнопках смайликах. Добавление кнопок(клавиатуры), согласно прототипу.
    +База данных заполнена выбранными тостами (Небольшие изменения в dataSourses из-за переподключения БД)

--- Bot/src/main/java/Bot.java
+++ Bot/src/main/java/Bot.java
@@ -1,10 +1,13 @@
+import com.vdurmont.emoji.EmojiParser;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -13,6 +16,28 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import java.util.ArrayList;
 
 //import static java.awt.DefaultKeyboardFocusManager.sendMessage;
+
+//Смайлики, используемые в сообщениях и кнопках
+enum Icon {
+
+    GLASSES(":clinking_glass:"),
+    STAR(":star:"),
+    CUP(":trophy:"),
+    WRITE(":writing_hand:"),
+    PEACE(":v:"),
+    HELP(":pray:");
+
+    private String value;
+
+    public String get() {
+        return EmojiParser.parseToUnicode(value);
+    }
+
+    Icon(String value) {
+        this.value = value;
+    }
+    }
+
 
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args){
@@ -32,6 +57,8 @@ public class Bot extends TelegramLongPollingBot {
         s.setChatId(message.getChatId().toString()); //Определяем, в какой чат отправляем ответ
         s.setReplyToMessageId(message.getMessageId()); //Определяем, на какое сообщение отвечаем
         s.setText(text);
+        //String msg = "SELECT value_toast FROM toasts ORDER BY RANDOM() limit 1;";
+        
         try{
             setButtons(s);
             execute(s);
@@ -70,14 +97,32 @@ public class Bot extends TelegramLongPollingBot {
         keyboardMarkup.setOneTimeKeyboard(false); //не скрывать клавиатуру
 
         ArrayList<KeyboardRow> keyboardRowList = new ArrayList<>();
-        KeyboardRow keyboardFstRow = new KeyboardRow();
+        KeyboardRow keyboard1Row = new KeyboardRow();
+        KeyboardRow keyboard2Row = new KeyboardRow();
+        KeyboardRow keyboard3Row = new KeyboardRow();
+        KeyboardRow keyboard4Row = new KeyboardRow();
+        KeyboardRow keyboard5Row = new KeyboardRow();
+        KeyboardRow keyboard6Row = new KeyboardRow();
 
-        keyboardFstRow.add(new KeyboardButton("/help")); //добавляем первую кнопку
-        keyboardFstRow.add(new KeyboardButton("/settings")); //добавляем вторую кнопку
+        keyboard1Row.add(new KeyboardButton(Icon.GLASSES.get() + "Мои тосты"));
+        keyboard2Row.add(new KeyboardButton(Icon.STAR.get() + "Избранное"));
+        keyboard3Row.add(new KeyboardButton(Icon.CUP.get() + "ТОП тостов"));
+        keyboard4Row.add(new KeyboardButton(Icon.WRITE.get() + "Добавить свой тост"));
+        keyboard5Row.add(new KeyboardButton(Icon.PEACE.get() + "Мой рейтинг"));
+        keyboard6Row.add(new KeyboardButton(Icon.HELP.get() + "Помощь"));
 
-        keyboardRowList.add(keyboardFstRow);
+        keyboardRowList.add(keyboard1Row);
+        keyboardRowList.add(keyboard2Row);
+        keyboardRowList.add(keyboard3Row);
+        keyboardRowList.add(keyboard4Row);
+        keyboardRowList.add(keyboard5Row);
+        keyboardRowList.add(keyboard6Row);
         keyboardMarkup.setKeyboard(keyboardRowList);
 
+        //Инлайн Клавиатура
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+        //inlineKeyboardButton.setText()
     }
 
     public String getBotUsername() {
