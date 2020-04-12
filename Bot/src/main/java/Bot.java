bbb37ed08beaacb9cc13593c232e744bad1235f5
Alessa666 <as2010alisa@gmail.com>
2020-04-12 12:52:06 +0500

    Инлайн клавиатура
    Добавлено сообщение с Профессиями и инлайн клавиатурой
    + изменен смайлик кнопки тостов

--- Bot/src/main/java/Bot.java
+++ Bot/src/main/java/Bot.java
@@ -14,13 +14,14 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 //import static java.awt.DefaultKeyboardFocusManager.sendMessage;
 
 //Смайлики, используемые в сообщениях и кнопках
 enum Icon {
 
-    GLASSES(":clinking_glass:"),
+    GLASS(":champagne:"),
     STAR(":star:"),
     CUP(":trophy:"),
     WRITE(":writing_hand:"),
@@ -51,39 +52,43 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    public void sendMsg(Message message, String text){
+    public void sendMessageReply (Message message, String text){
         SendMessage s = new SendMessage();
         s.enableMarkdown(true); //разметка
         s.setChatId(message.getChatId().toString()); //Определяем, в какой чат отправляем ответ
         s.setReplyToMessageId(message.getMessageId()); //Определяем, на какое сообщение отвечаем
         s.setText(text);
         //String msg = "SELECT value_toast FROM toasts ORDER BY RANDOM() limit 1;";
-        
         try{
-            setButtons(s);
+            setButtons(s); //появление клавиатуры после сообщения
             execute(s);
-
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-
     }
 
     //Имплементированый метод Bot
     //Для приема сообщений. Получение обновлений через LongPool (не через веб хук)
-
+    //Обработчик сообщений
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
         if(message != null && message.hasText()){
             switch(message.getText()){
                 case "/help":
-                    sendMsg(message, "Чем могу помочь?");
+                    sendMessageReply(message, "Чем могу помочь?");
                             break;
                 case "/settings":
-                        sendMsg(message, "Что будем настраивать?");
+                    sendMessageReply(message, "Что будем настраивать?");
                         break;
+                case "Привет!":
+                    try {
+                        execute(sendProfessionInlineKeyBoardMessage(update.getMessage().getChatId()));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 default:
-                    sendMsg(message, "Привет!");
+                    sendMessageReply(message, "Говно, отвечай нормально");
                     break;
             }
         }
@@ -104,7 +109,7 @@ public class Bot extends TelegramLongPollingBot {
         KeyboardRow keyboard5Row = new KeyboardRow();
         KeyboardRow keyboard6Row = new KeyboardRow();
 
-        keyboard1Row.add(new KeyboardButton(Icon.GLASSES.get() + "Мои тосты"));
+        keyboard1Row.add(new KeyboardButton(Icon.GLASS.get() + "Мои тосты"));
         keyboard2Row.add(new KeyboardButton(Icon.STAR.get() + "Избранное"));
         keyboard3Row.add(new KeyboardButton(Icon.CUP.get() + "ТОП тостов"));
         keyboard4Row.add(new KeyboardButton(Icon.WRITE.get() + "Добавить свой тост"));
@@ -118,11 +123,30 @@ public class Bot extends TelegramLongPollingBot {
         keyboardRowList.add(keyboard5Row);
         keyboardRowList.add(keyboard6Row);
         keyboardMarkup.setKeyboard(keyboardRowList);
+    }
 
-        //Инлайн Клавиатура
+    public static SendMessage sendProfessionInlineKeyBoardMessage(long chatId){
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
-        //inlineKeyboardButton.setText()
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("1").setCallbackData("Журналист")); //добавление кнопки без объявления переменной
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("2").setCallbackData("Строитель"));
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("3").setCallbackData("Программист"));
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("4").setCallbackData("Врач"));
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("5").setCallbackData("Полицейский"));
+        keyboardButtonsRow2.add(new InlineKeyboardButton().setText("6").setCallbackData("Учитель"));
+        keyboardButtonsRow2.add(new InlineKeyboardButton().setText("7").setCallbackData("Архитектор"));
+        keyboardButtonsRow2.add(new InlineKeyboardButton().setText("8").setCallbackData("Бухгалтер"));
+        keyboardButtonsRow2.add(new InlineKeyboardButton().setText("9").setCallbackData("Юрист"));
+        keyboardButtonsRow2.add(new InlineKeyboardButton().setText("10").setCallbackData("Дизайнер"));
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList); //добавляем кнопки в разметку инлайн клавиатуры
+        return new SendMessage().setChatId(chatId).setText("Выбери ПРОФЕССИЮ из списка:\n1. Журналист\n2. Строитель\n3. Программист\n4. Врач\n5. Полицейский\n6. Учитель\n7. Архитектор\n8. Бухгалтер\n9. Юрист\n10. Дизайнер").setReplyMarkup(inlineKeyboardMarkup); //добавлем разметку в сообщение
     }
 
     public String getBotUsername() {
